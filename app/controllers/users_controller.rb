4c70c5de8cf31a55173fb5de44d6bf57f8e5d290
ksk <ksk@ksknoMacBook-Pro.local>
2020-09-03 13:01:11 +0900

    Start Bookers_debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,45 @@
 class UsersController < ApplicationController
-def show 
-end
+ def show
+	@user =User.find(paramus[:id])
+ end
 
-def create
-end
+ def create
+	@users = User.new(user_params)
+	@users.user_id = current_user.id
+	@user.save
+	flash[:notice] = "Welcome! You have signed up successfully."
+	redirect_to user_path
+ end
 
-def index
-end
+ def index
+	@user = User.new
+	@Users = User.all
+ end
 
-def edit
-end
+ def edit
+	@user =User.find(params[:id])
+ end
 
-def update
-end
+ def update
+	@user =User.find(params[:id])
+	if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+   	else
+      render :edit
+    end
+ end
 
-
+private
+ def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+ end
+ def correct_user
+    user = User.find(params[:id])
+    if current_user != user
+      redirect_to user_path(current_user)
+    end
+ end
 
 
 
